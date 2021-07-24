9ec6bc49aa1023f889b4a5fbb0dccf7b8336cc72
Tang jiyi <37148354+Tangjiyi0416@users.noreply.github.com>
2021-07-24 16:17:21 +0800

    rm

--- Assets/Tangjiyi/Scripts/Skill/SkillRotate.cs
+++ Assets/Tangjiyi/Scripts/Skill/SkillRotate.cs
@@ -25,77 +25,79 @@ namespace SkillSystem
         {
             RaycastHit2D target = Physics2D.Raycast(t.position, Vector2.zero, 1, 1 << LayerMask.NameToLayer("Floor"));
             //Debug.Log(t.name);
+            t.SetParent(target.transform);
             target.transform.GetComponentInParent<MapSection>().Rotate(angle);
-            if (t.position.x > target.transform.position.x)
-            {
-                if (t.position.y > target.transform.position.y)
-                {
-                    switch (angle)
-                    {
-                        case 90:
-                            MovementManager.Instance.AddAction(3);
-                            break;
-                        case -90:
-                            MovementManager.Instance.AddAction(1);
-                            break;
-                        case 180:
-                            MovementManager.Instance.AddAction(3);
-                            MovementManager.Instance.AddAction(1);
-                            break;
-                    }
-                }
-                else
-                {
-                    switch (angle)
-                    {
-                        case 90:
-                            MovementManager.Instance.AddAction(0);
-                            break;
-                        case -90:
-                            MovementManager.Instance.AddAction(3);
-                            break;
-                        case 180:
-                            MovementManager.Instance.AddAction(0);
-                            MovementManager.Instance.AddAction(3);
-                            break;
-                    }
-                }
-            }
-            else
-            {
-                if (t.position.y > target.transform.position.y)
-                {
-                    switch (angle)
-                    {
-                        case 90:
-                            MovementManager.Instance.AddAction(1);
-                            break;
-                        case -90:
-                            MovementManager.Instance.AddAction(2);
-                            break;
-                        case 180:
-                            MovementManager.Instance.AddAction(1);
-                            MovementManager.Instance.AddAction(2);
-                            break;
-                    }
-                }
-                else
-                {
-                    switch (angle)
-                    {
-                        case 90:
-                            MovementManager.Instance.AddAction(2);
-                            break;
-                        case -90:
-                            MovementManager.Instance.AddAction(0);
-                            break;
-                        case 180:
-                            MovementManager.Instance.AddAction(2);
-                            MovementManager.Instance.AddAction(0);
-                            break;
-                    }
-                }
-            }
+            t.parent = null;
+            // if (t.position.x > target.transform.position.x)
+            // {
+            //     if (t.position.y > target.transform.position.y)
+            //     {
+            //         switch (angle)
+            //         {
+            //             case 90:
+            //                 MovementManager.Instance.AddAction(3);
+            //                 break;
+            //             case -90:
+            //                 MovementManager.Instance.AddAction(1);
+            //                 break;
+            //             case 180:
+            //                 MovementManager.Instance.AddAction(3);
+            //                 MovementManager.Instance.AddAction(1);
+            //                 break;
+            //         }
+            //     }
+            //     else
+            //     {
+            //         switch (angle)
+            //         {
+            //             case 90:
+            //                 MovementManager.Instance.AddAction(0);
+            //                 break;
+            //             case -90:
+            //                 MovementManager.Instance.AddAction(3);
+            //                 break;
+            //             case 180:
+            //                 MovementManager.Instance.AddAction(0);
+            //                 MovementManager.Instance.AddAction(3);
+            //                 break;
+            //         }
+            //     }
+            // }
+            // else
+            // {
+            //     if (t.position.y > target.transform.position.y)
+            //     {
+            //         switch (angle)
+            //         {
+            //             case 90:
+            //                 MovementManager.Instance.AddAction(1);
+            //                 break;
+            //             case -90:
+            //                 MovementManager.Instance.AddAction(2);
+            //                 break;
+            //             case 180:
+            //                 MovementManager.Instance.AddAction(1);
+            //                 MovementManager.Instance.AddAction(2);
+            //                 break;
+            //         }
+            //     }
+            //     else
+            //     {
+            //         switch (angle)
+            //         {
+            //             case 90:
+            //                 MovementManager.Instance.AddAction(2);
+            //                 break;
+            //             case -90:
+            //                 MovementManager.Instance.AddAction(0);
+            //                 break;
+            //             case 180:
+            //                 MovementManager.Instance.AddAction(2);
+            //                 MovementManager.Instance.AddAction(0);
+            //                 break;
+            //         }
+            //     }
+            // }
 
         }
     }
