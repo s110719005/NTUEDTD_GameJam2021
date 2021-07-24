521f50faf12bc4ba572dc6c6930c1e9a38c7870b
s110719005 <48974204+s110719005@users.noreply.github.com>
2021-07-25 02:04:10 +0800

    ADD ui manager

--- Assets/unicornn/Script/UIManager.cs
+++ Assets/unicornn/Script/UIManager.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
+    
+    [SerializeField]private RectTransform buttonPanel; //-1200 - -700
+    [SerializeField]private float buttonPanelInDuration = 1.0f;
+    [SerializeField]private Ease buttonPanelEaseIn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        buttonPanel.DOAnchorPosX(-700,buttonPanelInDuration,true).SetEase(buttonPanelEaseIn);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ClosePanel(){
+        buttonPanel.DOAnchorPosX(-1200,buttonPanelInDuration,true).SetEase(buttonPanelEaseIn);
     }
 }
