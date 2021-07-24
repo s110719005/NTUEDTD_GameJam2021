36a78af838444dcd8bbcb858f4422a9ac9500388
s110719005 <48974204+s110719005@users.noreply.github.com>
2021-07-24 17:34:23 +0800

    DELETE animation

--- Assets/unicornn/Script/Audio/AudioManager.cs
+++ Assets/unicornn/Script/Audio/AudioManager.cs
@@ -1,9 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public Sound[] sounds;
+    
     // Start is called before the first frame update
     void Start()
     {
